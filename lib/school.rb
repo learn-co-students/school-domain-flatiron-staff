1f37938f4f15642c53a69ed84b195088bdd0bd58
Emily Jennings <emily.jennings@flatironschool.com>
2018-07-20 21:42:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+
+
+class School
+  attr_accessor :name, :roster, :grade, :student
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    student.sort
+  end
+
+
+
+end
