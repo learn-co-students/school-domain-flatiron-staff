5810585f6358b91d72a42951399383396ed04625
Rupinder Dhillon <rupinder.dhillon@flatironschool.com>
2019-10-22 21:19:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+  attr_reader :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = Hash.new { |h, k| h[k] = [] }
+  end
+
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+
+  def grade(year)
+    @roster[year]
+  end
+
+  def sort
+    @roster.each do |grade, student_list|
+      student_list.sort!
+    end
+  end
+end
